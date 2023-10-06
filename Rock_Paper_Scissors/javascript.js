9b3e339f45c144c82ff3f41119f0dbda7b8a179d
RiteshtheSharma <72566311+RiteshtheSharma@users.noreply.github.com>
2023-10-06 13:33:15 +0530

    fix(formated code):

--- Rock_Paper_Scissors/javascript.js
+++ Rock_Paper_Scissors/javascript.js
@@ -2,72 +2,87 @@
 
 // toolBox to run game and know its condition using its methods
 class Game {
-    static #CHOICES =[`rock`,`paper`,`scissors`];
-    #gameStatus=null;
-    #gamestatusMsg='Game is not started';
-    constructor() {   
-      }
+  static #CHOICES = [`rock`, `paper`, `scissors`];
+  #gameStatus = null;
+  #gamestatusMsg = "Game is not started";
+  constructor() {}
+  // private method plz don 't use it outside this class
+  fetchComputerChoice() {
+    let index = parseInt(Math.random() * 3);
+    let compChoice = Game.#CHOICES[index];
+    return compChoice;
+  }
 
-      fetchComputerChoice(){
-       let index = parseInt(Math.random()*3);
-       let compChoice = Game.#CHOICES[index];   
-        return compChoice;
-    } 
-
-DecideWinner(playerSelection) {playerSelection = playerSelection.toLowerCase();
-const computerSelection = this.fetchComputerChoice();
-if (playerSelection === computerSelection) {this.#gamestatusMsg=`Its draw ! for ${playerSelection}`; this.#gameStatus=0}
-else if (Game.#CHOICES[(Game.#CHOICES.indexOf(playerSelection)+1)%3] === computerSelection ) 
-{this.#gamestatusMsg= `You lose ! for ${playerSelection} against ${computerSelection}`;
-this.#gameStatus=-1}
-else  { this.#gameStatus=1;this.#gamestatusMsg= `You won ! for ${playerSelection} against ${computerSelection}`;}
-}  
-
-get getGame_Status(){return this.#gameStatus}
-get getGame_Status_Msg(){return this.#gamestatusMsg}
-get getChoices_(){return Game.#CHOICES.toString()}
-} 
-// Class to use as an interface so user can play game using its object
-class  UserGameInterface{
-static #GAME_ALGO = new Game();
-static #times_won=0;
-static #times_loose=0;
-static #times_draw=0;
-static #userChoice;
-static #times_play;
-static #gameStatusArray=[];
-staticConstructor(times_play){
-    UserGameInterface.times_play = times_play;
-
-}
-
-PlayGame(userChoice){let gameStatus,gameStatusMsg;
-
-     
-    for(let i=0;i<UserGameInterface.times_play;i++){
-        
-        GAME_ALGO.DecideWinner(userChoice);
-        gameStatus = GAME_ALGO.getGame_Status;
-        gameStatusMsg = GAME_ALGO.getGame_Status_Msg; 
-        if(gameStatus===0)UserGameInterface.times_draw++;
-        else if(gameStatus===-1)UserGameInterface.times_loose++;
-        else UserGameInterface.times_won++;
-        UserGameInterface.#gameStatusArray.push(gameStatusMsg);
+  DecideWinner(playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    const computerSelection = this.fetchComputerChoice();
+    if (playerSelection === computerSelection) {
+      this.#gamestatusMsg = `Its draw ! for ${playerSelection}`;
+      this.#gameStatus = 0;
+    } else if (
+      Game.#CHOICES[(Game.#CHOICES.indexOf(playerSelection) + 1) % 3] ===
+      computerSelection
+    ) {
+      this.#gamestatusMsg = `You lose ! for ${playerSelection} against ${computerSelection}`;
+      this.#gameStatus = -1;
+    } else {
+      this.#gameStatus = 1;
+      this.#gamestatusMsg = `You won ! for ${playerSelection} against ${computerSelection}`;
     }
-}
-get getDetailedGameReport(){
-    return UserGameInterface.#gameStatusArray;
-}
-Result(){
-    return `times won ${UserGameInterface.#times_won}, times loose ${UserGameInterface.#times_loose} , times draw ${UserGameInterface.times_draw}`;
+  }
 
+  get getGame_Status() {
+    return this.#gameStatus;
+  }
+  get getGame_Status_Msg() {
+    return this.#gamestatusMsg;
+  }
+  get getChoices_() {
+    return Game.#CHOICES.toString();
+  }
 }
+// Class to use as an interface so user can play game using its object
+class UserGameInterface {
+  static #GAME_ALGO = new Game();
+  static #times_won = 0;
+  static #times_loose = 0;
+  static #times_draw = 0;
+  static #userChoice;
+  static #times_play;
+  static #gameStatusArray = [];
+  staticConstructor(times_play) {
+    UserGameInterface.times_play = times_play;
+  }
+
+  PlayGame(userChoice) {
+    let gameStatus, gameStatusMsg;
+
+    for (let i = 0; i < UserGameInterface.times_play; i++) {
+      GAME_ALGO.DecideWinner(userChoice);
+      gameStatus = GAME_ALGO.getGame_Status;
+      gameStatusMsg = GAME_ALGO.getGame_Status_Msg;
+      if (gameStatus === 0) UserGameInterface.times_draw++;
+      else if (gameStatus === -1) UserGameInterface.times_loose++;
+      else UserGameInterface.times_won++;
+      UserGameInterface.#gameStatusArray.push(gameStatusMsg);
+    }
+  }
+  get getDetailedGameReport() {
+    return UserGameInterface.#gameStatusArray;
+  }
+  Result() {
+    return `times won ${UserGameInterface.#times_won}, times loose ${
+      UserGameInterface.#times_loose
+    } , times draw ${UserGameInterface.times_draw}`;
+  }
 }
 
 // IIFEs to run game: https://www.javascripttutorial.net/javascript-immediately-invoked-function-expression-iife/
-()=>{  const VirtualGamingConsole = new UserGameInterface();
-    VirtualGamingConsole.staticConstructor(5);
-   let userChoice= prompt(' Enter your choice here among rock, paper and scissors ',''); 
-
-  }
-
+() => {
+  const VirtualGamingConsole = new UserGameInterface();
+  VirtualGamingConsole.staticConstructor(5);
+  let userChoice = prompt(
+    " Enter your choice here among rock, paper and scissors ",
+    ""
+  );
+};
